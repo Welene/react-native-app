5a5c3ea86a9a953265baccaf99e6e406631e8afb
Welene <helenetheo@live.no>
2026-03-10 16:36:01 +0100

    Initial push 2

---
+++ app/(tabs)/export.tsx
@@ -0,0 +1,11 @@
+// THIRD TAB SECTION
+
+import { Text, View } from 'react-native';
+
+export default function TabThreeScreen() {
+	return (
+		<View>
+			<Text>HELLO THIS IS TAB 3</Text>
+		</View>
+	);
+}
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,18 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+// MODAL SCREEN AKA
 
 export default function ModalScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
-  );
+	return <div className="Div example"> THIS IS THE MODAL SCREEN</div>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		alignItems: 'center',
+// 		justifyContent: 'center',
+// 		padding: 20,
+// 	},
+// 	link: {
+// 		marginTop: 15,
+// 		paddingVertical: 15,
+// 	},
+// });
